c56539535a44621f3eecbdd6c66fce46c5598578
jenicaliang <112435176+jenicaliang@users.noreply.github.com>
2024-11-13 00:21:30 -0600

    Add files via upload
    Initial commit for microsite initial build

---
+++ microsite/initial-build/js/js.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    if (document.getElementById("mia").src == "images/sub-beatcats-mia.png"){
+        document.getElementById("mia").src = "images/Mia_beatcats_vampire_transparent.png";
+    } else {
+        document.getElementById("mia").src = "images/Mia_beatcats_vampire_transparent.png";
+    }
+}
